2f89f4afd92e3a32a8556c2b07f54329d8392c49
dSalieri <paloshmax@gmail.com>
2021-10-02 02:05:19 +1000

    Tasks from lesson 2 have been completed

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,18 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  if (typeof path !== "string" || path[0] === ".") return null;
+  return function (obj) {
+    let pointer = 0;
+    for (let i = 0; i <= path.length; i++) {
+      if (path[i] === "." || i === path.length) {
+        if (!(obj = obj[path.slice(pointer, i)])) return;
+        pointer = ++i;
+      }
+      if (path[i] === "." && path[i + 1] === ".") {
+        break;
+      }
+    }
+    return obj;
+  };
 }
